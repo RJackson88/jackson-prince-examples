2e8d57cfc22a0476ba12b0bd900b30eaae4edd93
Rick Jackson <richardwjacksonjr@yahoo.com>
2017-09-16 01:15:54 -0400

    Initial Commit

---
+++ src/main/java/org/eightyeight/jackson/prince/JacksonPrinceExamplesApplication.java
@@ -0,0 +1,12 @@
+package org.eightyeight.jackson.prince;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonPrinceExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacksonPrinceExamplesApplication.class, args);
+	}
+}
